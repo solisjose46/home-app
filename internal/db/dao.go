5d2e0cfc3f8ec240c08b01295652571b0fd53d81
Jose Salazar <solisjose466@gmail.com>
2024-07-15 03:12:39 +0000

    hypermedia controls in templates implemented

--- internal/db/dao.go
+++ internal/db/dao.go
@@ -14,9 +14,9 @@ var dao *sql.DB
 func InitDB() error {
 	var err error
     dao, err = sql.Open("sqlite3", dbPath)
-    if err != nil {
-        return err
-    }
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
