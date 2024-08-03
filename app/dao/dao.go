ecf8046ad950d8d33cf875b88e8bc2751ec29d1d
Jose Salazar <solisjose466@gmail.com>
2024-08-03 01:50:45 +0000

    imported pretty-print and migrate dao debug

--- app/dao/dao.go
+++ app/dao/dao.go
@@ -6,8 +6,8 @@ import (
     "errors"
     "golang.org/x/crypto/bcrypt"
     "home-app/app/models"
-    "home-app/app/util"
     "time"
+    "github.com/solisjose46/pretty-print/debug"
 )
 
 const (
@@ -27,10 +27,10 @@ func InitDB() error {
 	var err error
     dao, err = sql.Open("sqlite3", dbPath)
 
-    util.PrintMessage("Connecting to db")
+    debug.PrintInfo(InitDB, "Connecting to db")
 
 	if err != nil {
-        util.PrintError(err)
+        debug.PrintError(InitDB, err)
 		return err
 	}
 
@@ -44,49 +44,49 @@ func CloseDB() {
 }
 
 func ValidateUser(username, password string) (bool, error) {
-    util.PrintMessage("Attempting to auth user ", username)
+    debug.PrintInfo(ValidateUser, "Attempting to auth user", username)
 
     var hashedPassword []byte
     err := dao.QueryRow(authUserQuery, username).Scan(&hashedPassword)
 
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(ValidateUser, err)
         return false, err
     }
 
     err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
     
     if err == bcrypt.ErrMismatchedHashAndPassword {
-        util.PrintError(err)
+        debug.PrintError(ValidateUser, err)
         return false, nil
     }
     
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(ValidateUser, err)
         return false, err
     }
 
-    util.PrintSuccess("Succ Auth ", username)
+    debug.PrintSucc(ValidateUser, "Succ Auth", username)
     return true, nil
 }
 
 func GetUserId(username string) (string, error) {
-    util.PrintMessage("getting user id")
+    debug.PrintInfo(GetUserId, "getting user id")
     var userId string
 
     err := dao.QueryRow(userIdQuery, username).Scan(&userId)
 
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(GetUserId, err)
         return "", err
     }
     
-    util.PrintMessage("got user id")
+    debug.PrintSucc(GetUserId, "got user id")
     return userId, nil
 }
 
 func AddExpense(expense models.Expense) (bool, error) {
-    util.PrintMessage("attempting to add expense")
+    debug.PrintInfo(AddExpense, "attempting to add expense")
 
     _, err := dao.Exec(
         addExpenseQuery, 
@@ -97,19 +97,16 @@ func AddExpense(expense models.Expense) (bool, error) {
     )
 
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(AddExpense, err)
         return false, err
     }
 
-    // what if no error and does not upload
-    // is that valid behavior
-
-    util.PrintSuccess("expense added!")
+    debug.PrintSucc(AddExpense, "expense added!")
     return true, nil
 }
 
 func UpdateExpense(expense models.Expense) (bool, error) {
-    util.PrintMessage("Attempting to update expense. expense: ", expense.Name)
+    debug.PrintInfo(UpdateExpense, "Attempting to update expense. expense:", expense.Name)
 
     _, err := dao.Exec(
         updateExpenseQuery, 
@@ -120,15 +117,16 @@ func UpdateExpense(expense models.Expense) (bool, error) {
     )
     
     if err != nil {
+        debug.PrintError(UpdateExpense, err)
         return false, err
     }
 
-    util.PrintSuccess("expense added!")
+    debug.PrintSucc(UpdateExpense, "expense added!")
     return true, nil
 }
 
 func GetExpensesForCurrentMonth(userId string) ([]models.Expense, error) {
-    util.PrintMessage("attempting to get monthly expense")
+    debug.PrintInfo(GetExpensesForCurrentMonth, "attempting to get monthly expense")
 
     now := time.Now()
     firstOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
@@ -138,7 +136,7 @@ func GetExpensesForCurrentMonth(userId string) ([]models.Expense, error) {
     startOfMonthStr := firstOfMonth.Format("2006-01-02 15:04:05")
     nextMonthStr := nextMonth.Format("2006-01-02 15:04:05")
 
-    util.PrintMessage("from ", startOfMonthStr, " to ", nextMonthStr)
+    debug.PrintInfo(GetExpensesForCurrentMonth, "from ", startOfMonthStr, " to ", nextMonthStr)
 
     rows, err := dao.Query(
         monthlyExpensesQuery,
@@ -147,7 +145,7 @@ func GetExpensesForCurrentMonth(userId string) ([]models.Expense, error) {
     )
 
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(GetExpensesForCurrentMonth, err)
         return nil, err
     }
     defer rows.Close()
@@ -166,10 +164,11 @@ func GetExpensesForCurrentMonth(userId string) ([]models.Expense, error) {
         )
 
         if err != nil {
-            util.PrintError(err)
+            debug.PrintError(GetExpensesForCurrentMonth, err)
             return nil, err
         }
 
+        // this seems messy
         if expense.ExpenseId == userId {
             expense.IsOwner = true
         } else {
@@ -178,17 +177,17 @@ func GetExpensesForCurrentMonth(userId string) ([]models.Expense, error) {
     }
 
     if err = rows.Err(); err != nil {
-        util.PrintError(err)
+        debug.PrintError(GetExpensesForCurrentMonth, err)
         return nil, err
     }
 
-    util.PrintSuccess("got monthly expenses")
+    debug.PrintSucc(GetExpensesForCurrentMonth, "got monthly expenses")
     return expenses, nil
 }
 
 
 func GetExpense(expenseId string) (models.Expense, error) {
-    util.PrintMessage("getting expense w/ id: ", expenseId)
+    debug.PrintInfo(GetExpense, "getting expense w/ id: ", expenseId)
     var expense models.Expense
 
     err := dao.QueryRow(getExpenseQuery, expenseId).Scan(
@@ -203,19 +202,20 @@ func GetExpense(expenseId string) (models.Expense, error) {
 
     if err != nil {
         if err == sql.ErrNoRows {
-            util.PrintError(err)
+            debug.PrintError(GetExpense, err)
             return expense, errors.New("expense not found")
         }
 
-        util.PrintError(err)
+        debug.PrintError(GetExpense, err)
         return expense, err
     }
 
-    util.PrintSuccess("expense found!")
+    debug.PrintSucc(GetExpense, "expense found!")
     return expense, nil
 }
 
 func GetCategoriesForCurrentMonth() ([]models.Category, error) {
+    debug.PrintInfo(GetCategoriesForCurrentMonth, "Getting categories")
     now := time.Now()
     firstOfMonth := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
     nextMonth := firstOfMonth.AddDate(0, 1, 0)
@@ -223,11 +223,9 @@ func GetCategoriesForCurrentMonth() ([]models.Category, error) {
     startOfMonthStr := firstOfMonth.Format("2006-01-02 15:04:05")
     nextMonthStr := nextMonth.Format("2006-01-02 15:04:05")
 
-
-
     rows, err := dao.Query(monthlyCategoriesQuery, startOfMonthStr, nextMonthStr)
     if err != nil {
-        util.PrintError(err)
+        debug.PrintError(GetCategoriesForCurrentMonth, err)
         return nil, err
     }
     defer rows.Close()
@@ -237,18 +235,17 @@ func GetCategoriesForCurrentMonth() ([]models.Category, error) {
         var category models.Category
         err := rows.Scan(&category.Name, &category.Balance, &category.Limit)
         if err != nil {
-            util.PrintError(err)
+            debug.PrintError(GetCategoriesForCurrentMonth, err)
             return nil, err
         }
         categories = append(categories, category)
     }
 
     if err = rows.Err(); err != nil {
-        util.PrintError(err)
+        debug.PrintError(GetCategoriesForCurrentMonth, err)
         return nil, err
     }
 
-    util.PrintSuccess("Got categories!")
+    debug.PrintSucc(GetCategoriesForCurrentMonth, "Got categories!")
     return categories, nil
 }
-
--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/gorilla/securecookie v1.1.2 // indirect
 	github.com/gorilla/sessions v1.3.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/solisjose46/pretty-print v1.0.0 // indirect
 	golang.org/x/crypto v0.25.0 // indirect
 )
